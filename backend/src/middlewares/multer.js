61d54229428b6546652796c47a01da4ebf11683a
Hameed Gul <hamidkhan31590@gmail.com>
2025-11-16 11:22:20 +0500

    fixing uploads

--- backend/src/middlewares/multer.js
+++ backend/src/middlewares/multer.js
@@ -1,20 +1,32 @@
-import multer from 'multer';
-import path from 'path';
+// import multer from 'multer';
+// import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Folder to save files
-  },
-  filename: function (req, file, cb) {
-    // Example: file-1234567890.jpg
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/'); // Folder to save files
+//   },
+//   filename: function (req, file, cb) {
+//     // Example: file-1234567890.jpg
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
+
+// export default upload;
+
+
+import multer from "multer";
+
+// Use memory storage for serverless environments
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage });
 
 export default upload;
+
+
 
 
 // import multer from "multer";
