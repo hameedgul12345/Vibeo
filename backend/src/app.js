5a73604a7c8ba5fb8e5521e9f89b21c08b7f7b69
Hameed Gul <hamidkhan31590@gmail.com>
2026-06-20 11:59:12 +0500

    adding netlify file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
@@ -68,3 +70,5 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
+
+
